976098b45e391c00bd01cfcdc2a5d9504b520662
Asad Rizvi <z5363026@ad..unsw.edu.au>
2025-10-13 16:15:53 +1100

    Pushed Lab 5

---
+++ lab5-main/src/pizza_cutter/build/pizza_cutter_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/asad/mtrn4231/lab5-main/src/pizza_cutter/pizza_cutter_description
+# Build directory: /home/asad/mtrn4231/lab5-main/src/pizza_cutter/build/pizza_cutter_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
