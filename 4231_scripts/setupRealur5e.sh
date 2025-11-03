09192e64268dc2b1ae99a396a29500c0582cceda
Joshua Cruzado <z5367218@ad.unsw.edu.au>
2025-11-03 12:09:35 +1100

    working gripper

--- 4231_scripts/setupRealur5e.sh
+++ 4231_scripts/setupRealur5e.sh
@@ -1,8 +1,12 @@
-gnome-terminal -t "DriverServer" -e 'ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur5e robot_ip:=192.168.0.100 use_fake_hardware:=false launch_rviz:=false'
+# Source both workspaces
+source /home/mtrn/4231/ros_ur_driver/install/setup.bash
+source /home/mtrn/Documents/mtrn4231_jakos/ros2_system/install/setup.bash
+
+gnome-terminal -t "DriverServer" -- bash -c 'source /home/mtrn/4231/ros_ur_driver/install/setup.bash && source /home/mtrn/Documents/mtrn4231_jakos/ros2_system/install/setup.bash && ros2 launch /home/mtrn/Documents/mtrn4231_jakos/4231_scripts/ur_control_fixed.launch.py ur_type:=ur5e robot_ip:=192.168.0.100 initial_joint_controller:=scaled_joint_trajectory_controller use_fake_hardware:=false launch_rviz:=false description_file:=ur5e_with_end_effector.urdf.xacro description_package:=motion_control_module; exec bash'
 
 sleep 10
 
-gnome-terminal -t "MoveitServer" -e 'ros2 launch ur_moveit_config ur_moveit.launch.py robot_ip:=192.168.0.100 ur_type:=ur5e launch_rviz:=true'
+gnome-terminal -t "MoveitServer" -- bash -c 'source /home/mtrn/4231/ros_ur_driver/install/setup.bash && source /home/mtrn/Documents/mtrn4231_jakos/ros2_system/install/setup.bash && ros2 launch /home/mtrn/Documents/mtrn4231_jakos/4231_scripts/ur_moveit_fixed.launch.py ur_type:=ur5e launch_rviz:=true use_fake_hardware:=false description_file:=ur5e_with_end_effector.urdf.xacro description_package:=motion_control_module; exec bash'
 
 
 
